67b606d51835adc84b10db7904ed4ac1e85f2e2b
pwnius <e42291@gmail.com>
2019-05-04 14:46:11 -0700

    Destruct props

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,14 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Card(props) {
-  const { img, name } = props.cardInfo;
-  console.log(props);
-  return <div className="card-container">{name}</div>;
+  const { cardInfo } = props;
+  const {
+    name,
+    cardSet,
+    type,
+    rarity,
+    cost,
+    attack,
+    health,
+    text,
+    playerClass,
+    img,
+    imgGold,
+  } = cardInfo;
+  return (
+    <div className="card-container">
+      <img src={img} alt={name} />
+    </div>
+  );
 }
 
 Card.propTypes = {
-  img: PropTypes.string,
-  name: PropTypes.string,
+  cardInfo: PropTypes.object,
 };
 
 export default Card;
